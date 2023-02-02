1e2a78dd92c3336066a82f00a38a3d6e1dccaaf4
Virkkunen <vrkknn@vrkknn.net>
2023-02-02 10:13:47 -0300

    estilizacao login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-max-depth */
 import PropTypes from 'prop-types';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Button, Container, Form, Row } from 'react-bootstrap';
 import { AuthContext } from '../context/AuthProvider';
+import rockGlass from '../images/rockGlass.svg';
+import layeredWaves from '../images/layeredWaves.svg';
 
 function Login({ history }) {
   const [loginData, setLoginData] = useState({
@@ -34,43 +37,63 @@ function Login({ history }) {
   }, [loginData]);
 
   return (
-    <Container>
-      <Row>
-        <h1>Login</h1>
-      </Row>
-      <Row>
-        <Form onSubmit={ handleSubmit }>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email"
-              data-testid="email-input"
-              value={ loginData.email }
-              name="email"
-              onChange={ handleChange }
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              data-testid="password-input"
-              value={ loginData.password }
-              name="password"
-              onChange={ handleChange }
-            />
-          </Form.Group>
-          <Button
-            variant="primary"
-            type="submit"
-            data-testid="login-submit-btn"
-            disabled={ btnDisabled }
-          >
-            Enter
-          </Button>
-        </Form>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <div className="meals">
+            <object
+              className="rocksGlass"
+              type="image/svg+xml"
+              data={ rockGlass }
+            >
+              Glass
+            </object>
+          </div>
+        </Row>
+        <Row>
+          <h1>Login</h1>
+        </Row>
+        <Row>
+          <Form onSubmit={ handleSubmit }>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                data-testid="email-input"
+                value={ loginData.email }
+                name="email"
+                onChange={ handleChange }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                data-testid="password-input"
+                value={ loginData.password }
+                name="password"
+                onChange={ handleChange }
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+              data-testid="login-submit-btn"
+              disabled={ btnDisabled }
+            >
+              Enter
+            </Button>
+          </Form>
+        </Row>
+      </Container>
+      <object
+        className="wave-bg"
+        type="image/svg+xml"
+        data={ layeredWaves }
+      >
+        Glass
+      </object>
+    </>
   );
 }
 
